97cb9ee90beb0ba17c7606a6d25e313bb6441a84
Lucas Rodriguez <lrodriguez.arevalosc@gmail.com>
2023-09-07 23:43:40 -0300

    add routes and app use

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import paymentRoutes from './routes/payment.routes.js'
 
 const app = express()
 
@@ -6,6 +7,10 @@ const app = express()
 
 app.listen(process.env.PORT || 3000, () => {})
 console.log('Server on Port', 3000);
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(paymentRoutes)
 
 app.get('/', (req, res) => {
     res.send('Hello World');})
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -2,6 +2,10 @@ import { Router } from "express";
 
 const router = Router();
 
-
 router.get("/create-order", (req, res) => res.send("creating order"))
+
+router.get("/success", (req, res) => res.send("success"))
+
+router.get("/wehook", (req, res) => res.send("webhook"))
+
 export default router
