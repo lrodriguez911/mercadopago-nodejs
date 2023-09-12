ce067d29c1a8ae094d29c6df872f41bc2dad5c54
Lucas Rodriguez <lrodriguez.arevalosc@gmail.com>
2023-09-11 23:23:45 -0300

    add mercadopago

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,4 +1,26 @@
-export const createOrder = (req, res) => res.send("creating order")
+import mercadopago from "mercadopago";
+
+
+export const createOrder = async (req, res) => {
+    
+    mercadopago.configure({
+        access_token: process.env.ACCESS_TOKEN
+    
+    })
+
+    const result = await mercadopago.preferences.create({
+        items: [
+            {
+                title: "jbl charge 5",
+                unit_price: 100,
+                currency_id: "ARS",
+                quantity: 1
+            }
+        ]
+    })
+
+    res.send("creating order")
+}
 
 export const success = (req, res) => res.send("success")
 
