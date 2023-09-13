cb1188c0b95456c1da401dff507b2caf583e0a49
Lucas Rodriguez <lrodriguez.arevalosc@gmail.com>
2023-09-12 23:50:14 -0300

    addd succer order

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -21,7 +21,27 @@ export const createOrder = async (req, res) => {
 
     res.send("creating order")
 }
+export const success = async (req, res) => {
+    
+    mercadopago.configure({
+        access_token: process.env.ACCESS_TOKEN
+    
+    })
 
-export const success = (req, res) => res.send("success")
+    const result = await mercadopago.preferences.create({
+        items: [
+            {
+                title: "jbl charge 5",
+                unit_price: 100,
+                currency_id: "ARS",
+                quantity: 1
+            }
+        ]
+    })
+
+    res.send("success  order")
+}
+
+// export const success = (req, res) => res.send("success")
 
 export const webhook = (req, res) => res.send("webhook");
